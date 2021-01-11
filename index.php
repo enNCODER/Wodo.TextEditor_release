04fbbf51d4cede35a81befb55eae4a913b86c77f
enNCODER <69395772+enNCODER@users.noreply.github.com>
2021-01-11 17:31:32 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <center>
-<img src"https://bong.surf/logo.png">
+<img src="https://bong.surf/logo.png">
 </center>
 <meta http-equiv="refresh" content="0.5; URL=http://docs.bong.surf/localeditor.html" />
